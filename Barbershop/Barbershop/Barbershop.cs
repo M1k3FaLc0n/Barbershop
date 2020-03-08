f2f68372c8d0dc9055668ddc20aae36636e18ddb
Mike <20vampir11@gmail.com>
2020-03-08 12:56:22 +0300

    added few classes and methods
    added service and barbershop classes + few services

---
+++ Barbershop/Barbershop/Barbershop.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barbershop
+{
+    static class Barbershop
+    {
+        public static List<Customer> customers = new List<Customer>();
+        public static List<Service> services = new List<Service>();
+
+        public static int find_customer(Customer customer)
+        {
+            for (int i = 0; i < customers.Count; i++)
+            {
+                if (customers[i].is_compare(customer))
+                    return i;
+            }
+            return -1;
+        }
+
+        public static int find_service(____)
+        {
+            for (int i = 0; i < ____; i++)
+            {
+                if (___)
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
--- Barbershop/Barbershop/Service.cs
+++ Barbershop/Barbershop/Service.cs
@@ -8,5 +8,33 @@ namespace Barbershop
 {
     class Service
     {
+        private int cur_max_id = 0;
+        private int id;
+
+        public string name;
+        public int price;
+
+        public Service(string name, int price) {
+            this.name = name;
+            this.price = price;
+            id = cur_max_id;
+            cur_max_id++;
+        }
+
+        public void print()
+        {
+            Console.WriteLine("____________");
+            Console.WriteLine("Услуга № " + id + ":");
+            Console.WriteLine("Название - " + name);
+            Console.WriteLine("Цена - " + price);
+            Console.WriteLine("____________");
+        }
+
+        public bool is_compare(Service service)
+        {
+            if (id == service.id && name == service.name && price == service.price)
+                return true;
+            return false;
+        }
     }
 }
