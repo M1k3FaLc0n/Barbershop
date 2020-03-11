021dcf519aeb205e23e79660bd7ddbf52e47bea8
Mike <20vampir11@gmail.com>
2020-03-11 16:15:15 +0300

    described whole barbershop class

--- Barbershop/Barbershop/Barbershop.cs
+++ Barbershop/Barbershop/Barbershop.cs
@@ -21,14 +21,68 @@ namespace Barbershop
             return -1;
         }
 
-        public static int find_service(____)
+        public static int find_service(Service service)
         {
-            for (int i = 0; i < ____; i++)
+            for (int i = 0; i < services.Count; i++)
             {
-                if (___)
+                if (services[i].is_compare(service))
                     return i;
             }
             return -1;
+        }
+
+        public static bool add_customer(Customer customer)
+        {
+            int customer_index = find_customer(customer);
+            if (customer_index != -1)
+                return false;
+            customers.Add(customer);
+            return true;
+        }
+
+        public static bool add_customer(string name, string last_name) {
+            return add_customer(new Customer(name, last_name));
+        }
+
+        public static bool add_service(Service service)
+        {
+            int service_index = find_service(service);
+            if (service_index != -1)
+                return false;
+            services.Add(service);
+            return true;
+        }
+
+        public static bool add_service(string name, int price)
+        {
+            return add_service(new Service(name, price));
+        }
+
+        public static void print()
+        {
+            if (customers.Count == 0)
+            {
+                Console.WriteLine("Клиентов пока нет :(");
+            }
+            else
+            {
+                Console.WriteLine("Клиенты: ");
+                for (int i = 0; i < customers.Count; i++)
+                    customers[i].print();
+            }
+
+            if (services.Count == 0)
+            {
+                Console.WriteLine("Услуг пока нет :(");
+            }
+            else
+            {
+                Console.WriteLine("Услуги: ");
+                for (int i = 0; i < services.Count; i++)
+                    services[i].print();
+            }
+
+
         }
     }
 }
